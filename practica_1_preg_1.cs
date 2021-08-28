b406e5547c0f14bc8b063d403b872cbb7072e748
Carlos Balcazar <cbalcazarp@gmail.com>
2021-08-28 07:58:35 -0500

    init

---
+++ practica_1_preg_1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace upn_myproject
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Double radio, longitudCircunferencia;
+            Console.Write("Ingrese radio de circunferencia: ");
+            radio = Convert.ToDouble(Console.ReadLine());
+            longitudCircunferencia = 2 * Math.PI * radio;
+            Console.WriteLine("La longitud de circunferencia con radio "+ Convert.ToString(radio) +" es: " + Convert.ToString(longitudCircunferencia));
+        }
+    }
+}
